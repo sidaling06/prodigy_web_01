ca8c0727cfbed9197f8df05626b4b87a6ce8db87
sidaling06 <siddumalaganvi108@gmail.com>
2025-05-08 22:28:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
